b252247620992b31707d3181f0530c0141421a31
Omer Salim <omersalim.tech@gmail.com>
2025-12-23 22:28:50 -0700

    Coded Horizon Travel Project

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,2 +1,84 @@
-console.log("a new line added");
-console.log("Second line added");
+const btnSearch = document.getElementById("btnSearch");
+const btnClear = document.getElementById("btnClear");
+const keywordInput = document.getElementById("keywordInput");
+const modalSearch = document.getElementById("modalSearch");
+
+const foundDestinations = [];
+
+function searchKeyword() {
+  modalSearch.innerHTML = "";
+
+  fetch("./travel_recommendation_api.json")
+    .then((response) => response.json())
+    .then((data) => {
+      foundDestinations.length = "";
+      for (destination in data) {
+        if (destination == "countries") {
+          for (country of data.countries) {
+            const foundCities = country.cities.filter((city) =>
+              city.name.toLowerCase().includes(keywordInput.value.toLowerCase())
+            );
+            // const foundCitiesWithTimeZone = foundCities.map((city) => ({...city, timeZone: getTimeZoneByCity(city.name.split(", ")[0]), })); // generating new array and new objects
+            foundCities.map((city) => { city.timeZone = getTimeZoneByCity(city.name.split(", ")[0]); return city; }); // mutating the original array
+            foundDestinations.push(...foundCities);
+          }
+        }
+        else if (destination.toLowerCase().includes(keywordInput.value.toLowerCase()))
+          foundDestinations.push(...data[destination]);
+      }
+      displayRecommendations();
+    })
+    .catch((error) => console.log("error while fetching data: ", error));
+}
+
+btnSearch.addEventListener("click", searchKeyword);
+
+function displayRecommendations() {
+  if (foundDestinations.length !== 0) {
+    for (destination of foundDestinations) {
+
+      if (destination.timeZone) {
+        const timeZoneDiv = document.createElement("div");
+        timeZoneDiv.classList.add("timeZoneDiv");
+        timeZoneDiv.innerHTML = `Current Local Time (${destination.name}): ${getTimebyTimeZone(destination.timeZone)}`
+        modalSearch.appendChild(timeZoneDiv);
+      }
+
+      const recommendationDiv = document.createElement("div");
+      recommendationDiv.classList.add("recommendation");
+      recommendationDiv.innerHTML += `<image src="${destination.imageUrl}"/>`;
+      recommendationDiv.innerHTML += `<h3>${destination.name}</h3>`;
+      recommendationDiv.innerHTML += `<p>${destination.description}</p>`;
+      recommendationDiv.innerHTML += `<button type="button">Visit</button>`;
+
+      modalSearch.appendChild(recommendationDiv);
+    }
+  } else {
+    modalSearch.innerHTML = `<p class="recommendation" style="padding: 5vh; text-align: center;">No matching destination</p>`;
+  }
+
+  modalSearch.showModal();
+}
+
+function getTimeZoneByCity(cityName) {
+  const formattedCity = cityName.trim().replace(/\s+/g, "_").toLowerCase(); // Standardize input: "new york" -> "New_York"
+  const allZones = Intl.supportedValuesOf("timeZone"); // Get all supported IANA timezones (e.g., ["Africa/Abidjan", "America/New_York", ...])
+  return allZones.find((zone) => zone.toLowerCase().endsWith(formattedCity)); // Find the first zone that ends with our city name
+}
+
+function getTimebyTimeZone(timeZone) {
+  const options = { timeZone, hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
+  const timeString = new Date().toLocaleTimeString("en-US", options)
+  return timeString;
+}
+
+modalSearch.addEventListener("click", (e) => {
+  // console.log(e.target)
+  if (e.target === modalSearch) {
+    modalSearch.close();
+    keywordInput.value = "";
+    keywordInput.focus();
+  }
+});
+
+btnClear.addEventListener("click", () => (keywordInput.value = ""));
